4af13377a26022832a27c22e47084edf969b1699
Admin <stiker1503@gmail.com>
2025-02-14 16:41:38 +0300

    applied isort

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from src.masks import get_mask_card_number, get_mask_account
-from src.widget import mask_account_card, get_date
-
-print(get_mask_card_number(7000792289606361))
-
-print(get_mask_account(73654108430135874305))
-
+from src.widget import get_date, mask_account_card
 
 print(mask_account_card("Maestro 1596837868705199"))
+
+print(mask_account_card("Счет 64686473678894779589"))
 
 print(get_date("2024-03-11T02:26:18.671407"))
