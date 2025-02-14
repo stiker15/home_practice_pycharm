816bf38cefb700c61bc31f1416f38089b6749151
Admin <stiker1503@gmail.com>
2025-02-14 16:21:56 +0300

    edits flake8 and mypy

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from src.masks import get_mask_card_number, get_mask_account
-
+from src.widget import mask_account_card, get_date
 
 print(get_mask_card_number(7000792289606361))
 
 print(get_mask_account(73654108430135874305))
 
 
-from src.widget import mask_account_card, get_date
-
-
-print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 1596837868705199"))
 
 print(get_date("2024-03-11T02:26:18.671407"))
